f24637872d22504f9cb0da6b34e26e4c02d186bf
Ashwin Jacob <ashjacob@gmail.com>
2022-02-27 23:14:49 -0600

    Commit of Final Product

---
+++ src/Post.js
@@ -0,0 +1,34 @@
+import './Post.css'
+import {Avatar} from "@mui/material";
+import InputOption from "./InputOption";
+import ThumbUpAltIcon from '@mui/icons-material/ThumbUpAlt';
+import AddCommentIcon from '@mui/icons-material/AddComment';
+import ShareIcon from '@mui/icons-material/Share';
+import SendIcon from '@mui/icons-material/Send';
+import {forwardRef} from "react";
+
+const Post  = forwardRef(({name, description, message, photoUrl}, ref) => {
+    return(
+        <div ref={ref} className='post'>
+            <div className='post__header'>
+                <Avatar src={photoUrl}>{name[0]}</Avatar>
+                <div className='post__info'>
+                    <h2>{name}</h2>
+                    <p>{description}</p>
+                </div>
+            </div>
+            <div className='post__body'>
+                <p>{message}</p>
+            </div>
+
+            <div className='post__buttons'>
+                <InputOption Icon={ThumbUpAltIcon} title='Like' color='gray '/>
+                <InputOption Icon={AddCommentIcon} title='Comment' color='gray '/>
+                <InputOption Icon={ShareIcon} title='Share' color='gray '/>
+                <InputOption Icon={SendIcon} title='Send' color='gray '/>
+            </div>
+        </div>
+    )
+})
+
+export default Post;
---
+++ src/Widgets.js
@@ -0,0 +1,29 @@
+import './Widgets.css'
+import InfoIcon from '@mui/icons-material/Info';
+import FiberManualRecordIcon from '@mui/icons-material/FiberManualRecord';
+
+function Widgets() {
+    const newsArticle = (heading, subtitle) => (
+        <div className='widgets__article'>
+            <div className='widgets__articleLeft'>
+                <FiberManualRecordIcon />
+            </div>
+            <div className='widgets__articleRight'>
+                <h4>{heading}</h4>
+                <p>{subtitle}</p>
+            </div>
+        </div>
+    );
+    return (
+        <div className='widgets'>
+            <div className='widgets__header'>
+                <h2>LinkedIn News</h2>
+                <InfoIcon />
+            </div>
+            {newsArticle('Ashwin Knows FE', "Top News")}
+            {newsArticle('React Native is Next', "Oranj")}
+        </div>
+    )
+}
+
+export default Widgets;
